19138c22dc38d2f56b40fe9f56685fa7e800de45
Robert Knight <robert.knight@mendeley.com>
2014-12-12 09:00:50 +0000

    Add example showing use of ts-style with React, TypeScript and typed-react

---
+++ examples/react-button/README.md
@@ -0,0 +1,8 @@
+# react-button example
+
+This example shows how to use TypeScript, React and ts-style
+together to build a trivial app with the component hierarchy,
+app logic and styling all implemented in TypeScript.
+
+This provides compile-time verification that all of the
+styles references are valid.
---
+++ examples/react-button/index.ts
@@ -0,0 +1,91 @@
+/// <reference path="node_modules/typed-react/typings/react/react.d.ts" />
+/// <reference path="node_modules/typed-react/dist/typed-react.d.ts" />
+/// <reference path="node_modules/ts-style/dist/ts-style.d.ts" />
+
+import react = require('react');
+import typed_react = require('typed-react');
+
+import style = require('ts-style');
+
+// use style.create() to create a set of
+// styles for use in components.
+//
+// The returned object has the same structure
+// as the input to style.create() but is augmented
+// with data needed by other style functions
+// (compile(), create(), mixin())
+var theme = style.create({
+	// styling for our top-level view.
+	// Elements that start with a lower-case
+	// name are translated to CSS classes,
+	// so this generates a '.app' selector
+	app: {
+		display: 'flex',
+
+		// camel-case property values are hyphenated in
+		// the generated CSS, so this becomes 'flex-direction'
+		flexDirection: 'column',
+
+		// numeric property values are suffixed with 'px'
+		// in the generated CSS
+		width: 100,
+
+		marginLeft: 'auto',
+		marginRight: 'auto'
+	},
+
+	button: {
+		backgroundColor: 'red',
+		border: '1px solid red',
+		borderRadius: 3,
+		color: 'white',
+		marginTop: 5,
+		marginBottom: 5,
+		padding: 5,
+		
+		// pseudo and descendant selectors can
+		// be created using properties that
+		// start with anything other than
+		// a lower-case letter
+		':active' : {
+			backgroundColor: 'green'
+		}
+	}
+});
+
+interface ButtonProps {
+	label: string;
+}
+
+class Button extends typed_react.Component<ButtonProps, {}> {
+	render() {
+		// use style.mixin() to merge the component's props
+		// with the props ('className', 'style') needed
+		// to apply styling from a theme element
+		return react.DOM.input(style.mixin(theme.button, {
+			type: 'button',
+			value: this.props.label
+		}));
+	}
+}
+var ButtonF = react.createFactory(typed_react.createClass(Button));
+
+class App extends typed_react.Component<{},{}> {
+	render() {
+		return react.DOM.div(style.mixin(theme.app),
+		  ButtonF({label: 'Button One'}),
+		  ButtonF({label: 'Button Two'})
+		);
+	}
+}
+var AppF = react.createFactory(typed_react.createClass(App));
+
+// check that we are running in a browser before trying
+// to render the component - this module is require'd by
+// ts-style in order to generate CSS from the style.create()
+// calls. An alternative approach is to put the style.create()
+// calls in a separate file which contains no app logic
+if (typeof document !== 'undefined') {
+	react.render(AppF({}), document.getElementById('app'));
+}
+
